ea957e65f51434f7585eeb268a333f093c71fc83
Sirakorn Lamyai <tanersirakorn@gmail.com>
2016-08-11 22:38:29 +0700

    Bootstrap added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,34 @@
   <title>
     Kasetsart University ICS File Generator
   </title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <style>
+    body { padding-top: 70px; }
+  </style>
 </head>
 <body>
-  <h1>
-    Kasetsart University ICS File Generator
-  </h1>
-  <hr/>
-  <p>Paste the source code from timetable page here</p>
-  <form method="POST" action="/file">
-    <textarea name="sourcecode"></textarea>
-    <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-    <button type="submit">Submit</button>
-  </form>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">KUtoICS</a>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="jumbotron">
+      <h1>
+        Kasetsart U. ICS File Generator
+      </h1>
+      <p>Copy the source code of <a href="https://std.regis.ku.ac.th/_Student_RptKu.php?mode=TIME_TABLE2">the timetable page</a>, and paste below, in order to generate the ICS file.</p>
+    </div>
+    <form method="POST" action="/file">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Paste your source code here</label>
+        <textarea name="sourcecode" class="form-control"></textarea>
+      </div>
+      <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </body>
 </html>
